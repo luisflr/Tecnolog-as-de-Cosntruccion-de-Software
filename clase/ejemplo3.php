1dbdc96142c2c65d7a787de91a01fd3704a30193
luisflr <37960514+luisflr@users.noreply.github.com>
2020-04-20 21:55:21 -0500

    Update ejemplo3.php

--- clase/ejemplo3.php
+++ clase/ejemplo3.php
@@ -1,29 +1,30 @@
 <?php
-
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-	CURLOPT_URL => "https://linguatools-conjugations.p.rapidapi.com/conjugate/?verb=eat",
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => array(
-		"x-rapidapi-host: linguatools-conjugations.p.rapidapi.com",
-		"x-rapidapi-key: 797e09afc8msh4b5628dcea4509cp168248jsn0aebeaf84a5a"
-	),
-));
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-	echo "cURL Error #:" . $err;
-} else {
-	echo $response;
+$filecontents = file_get_contents('palabras.txt');
+$palabras = preg_split('/[\s]+/', $filecontents, -1, PREG_SPLIT_NO_EMPTY); 
+$url = 'https://linguatools-conjugations.p.rapidapi.com/conjugate/';
+foreach  ($palabras as $key => $value){
+    $request_url = $url . '?verb=' . $value;
+    $curl = curl_init($request_url);
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            'x-rapidapi-host: linguatools-conjugations.p.rapidapi.com',
+            'x-rapidapi-key: 797e09afc8msh4b5628dcea4509cp168248jsn0aebeaf84a5a'
+        ),
+    ));
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    
+    $result = json_decode($response);
+    
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        echo json_encode($result);
+    }
+    next($palabras);
 }
+?>
