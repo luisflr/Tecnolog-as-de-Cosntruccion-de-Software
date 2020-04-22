d58080b9ff423e817018e5718fec53bc836d82ec
luisflr <37960514+luisflr@users.noreply.github.com>
2020-04-21 23:57:38 -0500

    Update ejemplo1.php

--- clase/ejemplo1.php
+++ clase/ejemplo1.php
@@ -1,31 +1,32 @@
 <?php
-    $cocina = Array("Frigider","Microondas","waflera");
-    $taller = Array("Llave","Martillo","alicate");
+    $cocina = Array("Frigider","Microondas","Waflera");
+    $taller = Array("Llave","Martillo","Alicate");
     $cuarto = Array("Escritorio","Cama","Comoda");
     $casa = Array($cocina,$taller,$cuarto);
     $casa1 = ['cocina','taller','cuarto'];   
 
-    // Descomentar... obtener los datos de un objeto
-    /*$tipoRoom=$_GET['tipo_room'];
-    echo json_encode($tipoRoom);
-    echo "<br>";
-    while ($nombre_lugar = current($casa1)) {
-        if ($nombre_lugar == $tipoRoom) {
-            $var = key($casa1);
-                echo json_encode($casa[$var],true);
-        }
-        next($casa1);
+    //Obtener los datos de un objeto
+    $tipoRoom=$_GET['tipo_room'];
+    if($tipoRoom=='cocina'||$tipoRoom=='taller'||$tipoRoom=='cuarto'){
+        echo json_encode($tipoRoom);
         
-    }*/
-    //Comentar ... obtener un objeto segun sus datos 
-    $producto=$_GET['producto'];
+        while ($nombre_lugar = current($casa1)) {
+            if ($nombre_lugar == $tipoRoom) {
+                $var = key($casa1);
+                    echo json_encode($casa[$var],true);
+            }
+            next($casa1);
+            
+        }
+    }
+    //obtener un objeto segun sus datos
     foreach($casa as $key => $value)
     {
         foreach($value as $clave => $valor){
-            if($producto == $valor){
+            if($tipoRoom == $valor){
                 $posicion = $key;
-                
-                echo json_encode($casa1[$posicion]);
+                echo "Pertenece a: ".json_encode($casa1[$posicion]);
+                //echo ;
             }
         }
     }
